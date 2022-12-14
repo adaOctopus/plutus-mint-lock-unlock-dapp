c368c6ee7cac5c76a4c309428691de58ff4a6795
gandalfg <gandalfg@vboxcardano.myguest.virtualbox.org>
2022-12-14 12:55:17 +0000

    Started NFT Identity policy script

---
+++ src/NFTIdent.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module NFTIdent where
+
+
+import           Cardano.Api as Cardano
+import           Cardano.Api                          (PlutusScriptV1,
+                                                       PlutusScriptV2,
+                                                       writeFileTextEnvelope, Script)
+import           Cardano.Api.Shelley                  (PlutusScript (..), PlutusScript(PlutusScriptSerialised), Address(..) )
+
+import qualified Codec.Serialise             as Codec
+import           Codec.Serialise
+import qualified Data.ByteString.Lazy                 as LBS
+
+import           GHC.Generics           ( Generic )
+import qualified Data.ByteString.Short                as SBS
+import qualified Data.ByteString.Base16 as B16
+import           Data.Functor                         (void)
+import           Data.Text                   (pack, unpack)
+import           Data.String                                    (fromString)
+import qualified Data.Maybe                           as DM (fromJust, fromMaybe)
+import qualified Ledger.Typed.Scripts                 as Scripts
+import           Ledger.Value                         as Value
+import qualified         Data.Aeson                  (decode, encode)
+import qualified Ledger.Address                       as LAD
+import qualified Plutus.Script.Utils.V1.Typed.Scripts as PSU.V1
+import qualified Plutus.Script.Utils.V2.Typed.Scripts as PSU.V2
+import qualified Plutus.Script.Utils.V2.Typed.Scripts.Validators as PSUV.V2
+import qualified Plutus.V1.Ledger.Api                 as PlutusV1
+import qualified Plutus.V1.Ledger.Contexts            as PlutusV1
+import qualified Plutus.V1.Ledger.Scripts             as PLV1
+import qualified Plutus.V2.Ledger.Api                 as PlutusV2
+import qualified Plutus.V2.Ledger.Contexts            as PlutusV2
+import           PlutusTx                             (getPir)
+import           Data.Aeson                  (decode, encode, FromJSON, ToJSON)
+import qualified PlutusTx
+import qualified PlutusTx.Builtins
+import           PlutusTx.Prelude                     as P hiding (Semigroup (..),unless, (.))
+import           Prelude                              (IO, (.), FilePath, Show, String, fromIntegral, show, div)
+import           Prettyprinter.Extras                 (pretty)
+import qualified PlutusTx.Prelude            as PPP (divide)
+import qualified Ledger                      as Plutus
+import           Cardano.Ledger.Credential   as Ledger
+
+--- THIS IS THE POLICY FOR THE NFT IDENTIFICATION TOKEN
+--- WHEN A USER DEPOSITS FUNDS TO THE LOCKSCRIPT RECEIVES IT
