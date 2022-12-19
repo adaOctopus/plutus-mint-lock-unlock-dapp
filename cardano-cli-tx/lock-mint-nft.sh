a05d16e08e9680a0e84330efca83941205457b75
gandalfg <gandalfg@vboxcardano.myguest.virtualbox.org>
2022-12-19 13:46:02 +0000

    Building cli transaction

--- cardano-cli-tx/lock-mint-nft.sh
+++ cardano-cli-tx/lock-mint-nft.sh
@@ -5,3 +5,6 @@ cardano-cli transaction build \
 	--tx-in-collateral 9c8ec6b8e92867383e153d8d6047c70714be12587b178fc34e9168c3d99c7b90#1 \
         --tx-in 9c8ec6b8e92867383e153d8d6047c70714be12587b178fc34e9168c3d99c7b90#1 \
 	--change-address $(cat ~/Cardano/plutus-vasil/plutus-mint-lock-unlock-dapp/test-addresses/01.addr) \
+	--tx-out $(cat ~/Cardano/plutus-vasil/plutus-mint-lock-unlock-dapp/lock-script.addr)+100000000 \
+	--tx-datum-file  ~/Cardano/plutus-vasil/plutus-mint-lock-unlock-dapp/jsons/datum-lock.json \
+	--out-file "lock-mint.body" 
